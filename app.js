b252369b9f034a569258bbe1f67bff528df4de2e
masonmike2001 <masonmike2001@gmail.com>
2023-01-27 09:10:08 -0500

    creates hourly and daily temp forecasts, and includes sample information

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 //include a live background that changes depending on the weather
+
+console.log(fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=0c1ce888b5b00b8dcae362794387f5bd'.main.temp_max));
